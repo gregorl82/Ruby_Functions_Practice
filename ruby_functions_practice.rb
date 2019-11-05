3ffd8e6b749b2acd1621a5d5c0b755cad70e853d
Gregor Lennie <gregorlennie@hotmail.co.uk>
2019-11-05 18:53:38 +0000

    Adds number_to_full_month_name function

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -32,3 +32,34 @@ end
 def add_string_as_number(first_number, second_number)
   return first_number.to_i + second_number.to_i
 end
+
+def number_to_full_month_name(number)
+  case number
+  when 1
+    return "January"
+  when 2
+    return "February"
+  when 3
+    return "March"
+  when 4
+    return "April"
+  when 5
+    return "May"
+  when 6
+    return "June"
+  when 7
+    return "July"
+  when 8
+    return "August"
+  when 9
+    return "September"
+  when 10
+    return "October"
+  when 11
+    return "November"
+  when 12
+    return "December"
+  else
+    return "Not a number that corresponds to a month"
+  end
+end
