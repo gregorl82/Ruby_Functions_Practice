f1e5167b0297aa9f228cd75dc53863c9a09b3102
Gregor Lennie <gregorlennie@hotmail.co.uk>
2019-11-05 18:30:16 +0000

    Adds subract function

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -1,8 +1,14 @@
 
+# MVP
+
 def return_10
   return 10
 end
 
 def add(first_number, second_number)
   return first_number + second_number
+end
+
+def subtract(first_number, second_number)
+  return first_number - second_number
 end
